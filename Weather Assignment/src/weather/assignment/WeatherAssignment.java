cb66106553cc136554e8944e79922a9ffaae9b19
cinikobr <cinikobr@sheridancollege.ca>
2022-02-03 11:37:28 -0500

    Added method calcMax.

--- Weather Assignment/src/weather/assignment/WeatherAssignment.java
+++ Weather Assignment/src/weather/assignment/WeatherAssignment.java
@@ -4,13 +4,13 @@ package weather.assignment;
  *
  * @author rck20
  */
-
 //HELLO THIS IS ME EDITING FROM GITHUB.COM
 public class WeatherAssignment {
 
     public static void main(String[] args) {
         int[] temps = {5, 8, -11, -14, -6, -3, -2, -7, -1, 1, 3, 4, 0, -3};
         System.out.println(calcAvg(temps));
+        System.out.println(calcMax(temps));
     }
 
     public static double calcAvg(int[] values) {
@@ -24,5 +24,16 @@ public class WeatherAssignment {
 
         }
         return sum / values.length;
+    }
+
+    public static int calcMax(int[] values) {
+        int max = values[0];
+        for (int value : values) {
+            if (value > max) {
+                max = value;
+            }
+
+        }
+        return max;
     }
 }
