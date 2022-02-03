6dad3624d4097609b400dc675bb0f572f7427c1b
cinikobr <cinikobr@sheridancollege.ca>
2022-02-03 11:28:51 -0500

    Created WeatherAssignment.

---
+++ Weather Assignment/src/weather/assignment/WeatherAssignment.java
@@ -0,0 +1,26 @@
+package weather.assignment;
+
+/**
+ *
+ * @author rck20
+ */
+public class WeatherAssignment {
+
+    public static void main(String[] args) {
+        int[] temps = {5, 8, -11, -14, -6, -3, -2, -7, -1, 1, 3, 4, 0, -3};
+        System.out.println(calcAvg(temps));
+    }
+
+    public static double calcAvg(int[] values) {
+        double sum = 0;
+        double count = 0;
+        for (int value : values) {
+            if (value >= -50 && value <= 50) {
+                sum += value;
+                count++;
+            }
+
+        }
+        return sum / values.length;
+    }
+}
